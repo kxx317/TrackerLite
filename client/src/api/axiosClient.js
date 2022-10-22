3263e2111ddfecaf7d45d9c57f655aca5b7e5481
Kangxing Xie <kangxing.xie@student.unsw.edu.au>
2022-10-22 16:37:38 +1100

    added new gitignore

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import queryString from "query-string";
 
-// const baseUrl = "http://localhost:8080/api/v1/";
-const baseUrl = "http://kanban-api.kangxingxie.me:8080/api/v1/";
+const baseUrl = "http://localhost:8080/api/v1/";
+// const baseUrl = "http://kanban-api.kangxingxie.me:8080/api/v1/";
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
